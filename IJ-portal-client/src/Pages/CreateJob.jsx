3bd915bcf3454e52dd3cd38d48a9229e59db1b7a
jathinmahesh <jathinmahesh@gmail.com>
2024-04-21 15:18:24 +0530

    Bug_fix

--- IJ-portal-client/src/Pages/CreateJob.jsx
+++ IJ-portal-client/src/Pages/CreateJob.jsx
@@ -13,7 +13,7 @@ const CreateJob = () => {
   const onSubmit = (data) => {
     data.skills = selectedOption;
     // console.log(data);
-    fetch("http://localhost:3000/post-job", {
+    fetch("http://localhost:3005/post-job", {
       method: "POST",
       headers: {"content-Type" : "application/json"},
       body: JSON.stringify(data)
